ed4593d8bc92587308c561ea58c1a9ed599cbd33
Paul Njogu <paulnjpgu2006@gmail.com>
2024-02-19 20:37:31 +0300

    added fasfforty files to repository

--- project.js
+++ project.js
@@ -1,5 +1,13 @@
-let menuIcon=document.getElementById('menuIcon');
-function displayNav(){
-let subPages=document.getElementById('subPages');
-subPages.
+function showSidebar(){
+  const sidebar=document.querySelector('.mobileSubpages');
+  sidebar.style.display='flex';
 }
+function hideSidebar(){
+  const sidebar=document.querySelector('.mobileSubpages');
+  sidebar.style.display='none';
+}
+
+let hamburgerMenu = document.querySelector('.toggleOn');
+let closeMenu = document.querySelector('.toggleOff');
+hamburgerMenu.addEventListener('click',showSidebar);
+closeMenu.addEventListener('click',hideSidebar);
